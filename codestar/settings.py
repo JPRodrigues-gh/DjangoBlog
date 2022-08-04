40559bd9fdbff754a035b0bd30d5b0ef18e30909
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-08-04 23:28:13 +0000

    Fix bug in settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -143,8 +143,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 # Media files
 MEDIA_URL = '/media/'
