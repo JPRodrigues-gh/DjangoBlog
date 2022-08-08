c8b93c5aa46b5f3d6158b1e36dccf2e74f866b0c
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-08-08 08:42:29 +0000

    Add commenting functionality with crispy forms

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
@@ -46,6 +47,44 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
-                "liked": liked
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *arg, **kwargs):
+        # Fetch only active posts
+        queryset = Post.objects.filter(status=1)
+        # Fetch the object using the unique field "slug"
+        post = get_object_or_404(queryset, slug=slug)
+        # Fetch only approved comments, order by date created ascending
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        # filter post likes by the user id to see if they've liked the post
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked=True
+        
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        # send all of the above information to our render method
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
             },
         )
