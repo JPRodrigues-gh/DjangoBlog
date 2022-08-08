69d4e327f77c0404b98adfad04d14e9739285a20
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-08-08 11:24:34 +0000

    Add like toggle on/off in view and template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 """ views.py in blog folder """
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
@@ -88,3 +89,17 @@ class PostDetail(View):
                 "comment_form": CommentForm()
             },
         )
+
+class PostLike(View):
+    """
+    Class for posting user likes
+    """
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
